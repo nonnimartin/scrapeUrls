21eb5149e9a566c69f6e973c590bcf788f7f2844
nonnimartin <nonnimartin@gmail.com>
2017-04-11 20:30:02 -0700

    Made object for getDomain

--- get_domains.py
+++ get_domains.py
@@ -1,63 +1,64 @@
 from bs4 import BeautifulSoup as soupy
 import requests
 
-def remove_prefix(text, prefix):
+class getDomains(object):
 
-    prefixes = ["/search?ie=UTF-8&q=related:", "/url?q="]
+    def __init__(self):
 
-    #if text.startswith(prefixes):
-    for prefix in prefixes:
-        if text.startswith(prefix):
-            split_text = text.split(prefix)
-            return split_text[1]
-    else:
-        return text
+        self.domains  = []
 
-def getLinks(url):
+    def remove_prefix(self, text):
 
-    #Get http request for URL
-    req = requests.get(url)
-    #Get text returned from request object
-    data = req.text
-    #Make BeautifulSoup HTML object
-    soupObj = soupy(data, "html.parser")
+        prefixes = ["/search?ie=UTF-8&q=related:", "/url?q="]
 
-    #raw image links from bs4
-    rawLinks=[]
+        #if text.startswith(prefixes):
+        for prefix in prefixes:
+            if text.startswith(prefix):
+                split_text = text.split(prefix)
+                return split_text[1]
+        else:
+            return text
 
-    #Links with markers of interest
-    links = []
-    
-    #list of normal default Google links that will occur in any basic Google search
-    defaultGoogle = [
+    def getLinks(self, url):
 
-                   "https://www.google.com/search?hl=en&tbm=isch&source=og&tab=wi",
-                   "https://maps.google.com/maps?hl=en&tab=wl",
-                   "https://play.google.com/?hl=en&tab=w8",
-                   "https://play.google.com/?hl=en&tab=w8",
-                   "https://www.youtube.com/results?tab=w1",
-                   "https://news.google.com/nwshp?hl=en&tab=wn",
-                   "https://mail.google.com/mail/?tab=wm",
-                   "https://drive.google.com/?tab=wo",
-                   "https://www.google.com/intl/en/options/",
-                   "http://www.google.com/history/optout?hl=en"
-    ]
+        #Get http request for URL
+        req = requests.get(url)
+        #Get text returned from request object
+        data = req.text
+        #Make BeautifulSoup HTML object
+        soupObj = soupy(data, "html.parser")
 
-    for link in soupObj.find_all('a'):
-        rawLinks.append(link.get('href'))
-    
-    #list features of interest
-    markers = ["http", "https", "www"]
+        #raw image links from bs4
+        rawLinks=[]
 
-    for link in rawLinks:
-        for marker in markers:
-            if marker in link and not any(substring in link for substring in defaultGoogle):
-                no_pref_link = remove_prefix(link, "/url?q=")
-                links.append(no_pref_link)
+        #Links with markers of interest
+        links = []
+        
+        #list of normal default Google links that will occur in any basic Google search
+        defaultGoogle = [
 
-    return links
+                       "https://www.google.com/search?hl=en&tbm=isch&source=og&tab=wi",
+                       "https://maps.google.com/maps?hl=en&tab=wl",
+                       "https://play.google.com/?hl=en&tab=w8",
+                       "https://play.google.com/?hl=en&tab=w8",
+                       "https://www.youtube.com/results?tab=w1",
+                       "https://news.google.com/nwshp?hl=en&tab=wn",
+                       "https://mail.google.com/mail/?tab=wm",
+                       "https://drive.google.com/?tab=wo",
+                       "https://www.google.com/intl/en/options/",
+                       "http://www.google.com/history/optout?hl=en"
+        ]
 
-allLinks = getLinks("https://www.google.com/search?q=test&oq=test&aqs=chrome..69i57j0l5.445j0j8&sourceid=chrome&ie=UTF-8")
+        for link in soupObj.find_all('a'):
+            rawLinks.append(link.get('href'))
+        
+        #list features of interest
+        markers = ["http", "https", "www"]
 
-for i in allLinks:
-    print i
+        for link in rawLinks:
+            for marker in markers:
+                if marker in link and not any(substring in link for substring in defaultGoogle):
+                    no_pref_link = self.remove_prefix(link)
+                    self.domains.append(no_pref_link)
+
+        return self.domains
